6945b8f6588a80a7d5cb32124ec5cae2b089b7bc
abhaypatro <47801727+abhaypatro@users.noreply.github.com>
2019-08-19 20:07:20 +0530

    updated test.py

--- test.py
+++ test.py
@@ -1,29 +1,4 @@
-C:\Users\abhay\Desktop\third sem# import random
-# import urllib.request
-#
-# def download_web_image(url):
-#     name=random.randrange(1,1000)
-#     full_name=str(name)+".jpg"
-#     urllib.request.urlretrieve(url, full_name)
-#
-# download_web_image("https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fthumb%2F7%2F7a%2FSpider-Man_3%252C_International_Poster.jpg%2F220px-Spider-Man_3%252C_International_Poster.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FSpider-Man_3&docid=Y_T5-dQRGJJXjM&tbnid=ZpTVX6icu8ehkM%3A&vet=10ahUKEwiX1sjzk4fkAhWGbisKHQq9C1MQMwh8KAAwAA..i&w=220&h=327&bih=743&biw=1536&q=spider%20man%203&ved=0ahUKEwiX1sjzk4fkAhWGbisKHQq9C1MQMwh8KAAwAA&iact=mrc&uact=8")
-# import os
-#
-#
-# fileSizeTupleList = []
-# largestSize = 0
-#
-# for i in os.listdir(os.curdir):
-#     if os.path.isfile(i):
-#         fileSizeTupleList.append((i, os.path.getsize(i)))
-#
-# for fileName, fileSize in fileSizeTupleList:
-#     if fileSize > largestSize:
-#         largestSize = fileSize
-#         largestFile = fileName
-#
-# print(largestFile, largestSize)
-#
+
 import os
 
 fileSizeList = []
@@ -40,8 +15,3 @@ for fileName, fileSize in fileSizeList:
 
 print(largestFile, "is the largest file with a size of ", largestSize,"bytes")
 
-# abhay=[]
-# abhay.append(("a","z"))
-# abhay.append(("b","y"))
-# for x, v in abhay:
-#     print(x + v)
